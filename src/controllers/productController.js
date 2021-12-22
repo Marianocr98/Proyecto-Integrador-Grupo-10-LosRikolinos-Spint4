fd2168abaee632435e7e4a22e0391883f29a6cc6
Marcelo Olivera <olivera.m.et13@gmail.com>
2021-12-22 17:44:44 -0300

    Vista para edicion de productos creada y mejora de la vista de subida de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -18,55 +18,67 @@ const productController = {
 
         res.render('./productos/productos' ,{productos:products})
     },
-
+    all:(req,res)=>{
+        res.render('./productos/productos' ,{productos:products})
+    },
+    formCreate: (req,res)=>{
+        res.render('./admin/admin')
+    },
+    productEdition : (req,res)=>{
+        res.render( './admin/productEdition')
+    },
+    edit : (req,res)=>{
+        const products = productModel.productos();
+    let id = req.params.id
+    for(let i = 0 ;  i < products.length; i++){
+          let idProducts = productModel[i].id
+        if( idProducts == id){
+            products[i].title = req.body.title
+            products[i].category = req.body.category
+            products[i].price = req.body.price
+        }
+       }
+    },
+    delete : (req,res)=>{
+        const products = productModel.productos();
+        let id = req.params.id
+        products = products.filter(product =>{
+        let idProducts = product.id
+        return   id  != idProducts
+        })
+    },
     productDetail: (req, res) => {
-
         let detalle = productModel.find(req.params.id);
-
         res.render('./productos/productDetail', {detail: detalle});
     },
     productsEmpanadas: (req, res) => {
-
         const productsEmpanadas = productModel.empanadas();
-
         res.render('./productos/productEmpanada', {empanada: productsEmpanadas});
     },
     productsHamburguer : (req,res) => {
-
         const productsHamburguesa = productModel.hamburguesa()
-
         res.render('./productos/productHamburguer',{productsHamburguer : productsHamburguesa })
     },
     productsPizza : (req,res) => {
-
         const productsPizza = productModel.pizza();
-
         res.render('./productos/productPizza',{productPizza : productsPizza})
     },
     productsPastas : (req,res) => {
-
         const productsPastas = productModel.pasta();
-
         res.render('./productos/productPastas',{pastas : productsPastas })
     },
     productsAsado : (req,res) => {
-
         const productsAsado = productModel.parrilla();
-
         res.render('./productos/productAsado',{asado : productsAsado })
     },
     productsBebidas : (req,res) => {
-
         const productsDrinks = productModel.bebidas();
-
         res.render('./productos/bebidas',{drink : productsDrinks })
     },
     shoppingCart : (req,res)=>{
         res.render('./productos/shopping-cart');
     }, 
-    admin: (req,res)=>{
-        res.render('./admin/admin');
-    },
+
 }
 
 
--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -3,10 +3,39 @@ const router = express.Router();
 
 const productController = require('../controllers/productController')
 
+
+//-- Listado de productos
+
 router.get('/productos', productController.productos)
+
+// Acción de creación (a donde se envía el formulario)-check
+router.post('/productos', productController.all)
+
+//-- Formulario de creación de productos
+
+router.get('/productos/create', productController.formCreate)
+
+//-- Detalle de un producto particular
+
 router.get('/productDetail/:id', productController.productDetail);
+// Formulario de edición de productos
+router.get('/productDetail/:id/edit', productController.productEdition);
+
+
+router.post('/productDetail/:id', productController.productDetail);
+
+// Acción de edición (a donde se envía el formulario):
+router.put('/productDetail/:id/', productController.edit);
+
+router.delete('/productDetail/:id', productController.delete);
+
+
+
 router.get('/shopping-cart', productController.shoppingCart);
-router.get('/admin', productController.admin);
+
+
+// -- Productos que iran en menu
+
 router.get('/productos/Pizza', productController.productsPizza);
 router.get('/productos/Hamburguesa', productController.productsHamburguer);
 router.get('/productos/Asado', productController.productsAsado);
